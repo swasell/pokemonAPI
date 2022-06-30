3c59106c5163ce1ce4fd06b236147702e27856d9
swasell <svenwasell@gmail.com>
2022-06-29 20:33:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [pokemonData, setPokemonData] = useState ([]);
+
+    useEffect(() => {
+      axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+          .then(response => {setPokemonData(response.data.results)})
+    }, []);
+
+
+    return (
+      <div className="App" style = {{width: '200px', margin: 'auto'}}> 
+        <ul>
+          {pokemonData.map((pokemonObj, index) =>{
+            return (<li key={index}>{pokemonObj.name}</li>)
+          })
+        }
+        </ul>
+      </div>
+    );
+  }
+
 
 export default App;
